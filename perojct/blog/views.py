cc2eb8d6051041368e750501415f3e4d882d9199
nischay-chauhan <masternischay@outlook.com>
2023-10-03 20:11:31 +0530

    Post model

--- perojct/blog/views.py
+++ perojct/blog/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {'author': 'CoreyMS', 'title': 'Blog Post 1', 'content': 'First post content', 'date_posted': 'April 20, 2018'},
-    {'author': 'Jane Doe', 'title': 'Blog Post 2', 'content': 'Second post content', 'date_posted': 'April 21, 2018'},
-    {'author': 'John Doe', 'title': 'Blog Post 3', 'content': 'Third post content', 'date_posted': 'April 22, 2018'},
-    {'author': 'Jane Doe', 'title': 'Blog Post 4', 'content': 'Fourth post content', 'date_posted': 'April 23, 2018'},
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request , 'blog/home.html' , context)
 def about(request):
